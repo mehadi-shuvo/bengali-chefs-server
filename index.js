4185e603a81809ded97df67e4858aacd7547acb9
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-05-03 06:59:12 +0600

    load data and install cors

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const chefs = require('./data/chefs.json');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+
+app.use(cors());
 app.get('/',(req, res)=>{
     res.send('hello chefs');
+})
+app.get('/chefs', (req, res)=>{
+    res.send(chefs);
+
 })
 app.listen(port, ()=>{
     console.log(`our local port is ${port}`);

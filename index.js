0f83c0d553c18ba6fe4e1e1b57fdc8aea5f8649f
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-05-03 07:09:19 +0600

    write a function for load data id wais

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/chefs', (req, res)=>{
     res.send(chefs);
 
 })
+app.get("/chefs/:id", (req, res)=>{
+    const id = req.params.id;
+    const chef = chefs.find(ch=> parseInt(ch.id)===parseInt(id));
+    res.send(chef);
+})
 app.listen(port, ()=>{
     console.log(`our local port is ${port}`);
 })
